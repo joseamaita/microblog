cdd5c6bb9e1da03150660f4213daca0b450f3d0d
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2018-06-07 23:23:04 -0400

    Update basic development procedure (working with pagination)
      This adds:
    
      - Section that shows you how to add pagination links in the user profile page.
      - Code that removes fake posts and does pagination in the user profile view function (app/routes.py)
      - The user profile template that displays pagination links (app/templates/user.html)
      - Code to set the "POSTS_PER_PAGE = 25" configuration variable (config.py)
      - Screenshots (08-pagination-[k,l].png)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-# app/routes.py: Next and previous page links
+# app/routes.py: Pagination in the user profile view function
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
@@ -80,11 +80,18 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'}, 
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', 
+                           user = user, 
+                           posts = posts.items, 
+                           next_url = next_url, 
+                           prev_url = prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
